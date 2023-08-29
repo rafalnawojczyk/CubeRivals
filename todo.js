ea9a9f52712ceae36649458117f3ea68255a2744
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-08-29 20:14:02 +0200

    Add image background and linear gradient at the same time

--- todo.js
+++ todo.js
@@ -1,1 +1,3 @@
 // TODO: Splash screen should be really looking good. It should have some kind of fading effect, animation should start when all assets are done fetching. Should use animated to do this.
+
+// TODO: Add internationalization to app and i18n support for multilang in future.
