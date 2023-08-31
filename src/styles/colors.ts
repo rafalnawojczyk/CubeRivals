10351a6ef076a05ef32f86e596f0937751b224a8
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-08-31 11:15:32 +0200

    Update colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,26 +1,35 @@
 export interface ColorsInterface {
-    primary: string;
+    background: string;
+    primary500: string;
+    primary800: string;
     secondary: string;
     tertiary: string;
     gray800: string;
+    gray700: string;
     gray400: string;
     text: string;
 }
 
 export const LIGHT_THEME: ColorsInterface = {
-    primary: '#582fff',
+    background: '#131417',
+    primary500: '#582fff',
+    primary800: '#2d1686',
     secondary: '#254B5A',
     tertiary: '#5DA6A7',
     gray800: '#1d1f24',
+    gray700: '#23252b',
     gray400: '#676d75',
     text: '#ffffff',
 };
 
 export const DARK_THEME: ColorsInterface = {
-    primary: '#582fff',
+    background: '#131417',
+    primary500: '#582fff',
+    primary800: '#2d1686',
     secondary: '#254B5A',
     tertiary: '#5DA6A7',
     gray800: '#1d1f24',
+    gray700: '#23252b',
     gray400: '#676d75',
     text: '#ffffff',
 };
