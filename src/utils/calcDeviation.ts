8a87d8c4a38c9e495053279983c43e7f63f0f49d
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-08-31 11:53:20 +0200

    Add deviation calc function

--- src/utils/calcDeviation.ts
+++ src/utils/calcDeviation.ts
@@ -1,3 +1,14 @@
-export const calcDeviation = () => {
-    // TODO: https://sciencing.com/calculate-cv-values-6395229.html
+import { calcAvg } from './calcAvg';
+
+export const calcDeviation = (times: number[]) => {
+    const mean = calcAvg(times, false);
+    let deviationSum = 0;
+
+    for (let i = 0; i <= times.length - 1; i++) {
+        deviationSum += Math.abs(times[i] - mean);
+    }
+
+    const deviation = deviationSum / times.length;
+
+    return deviation;
 };
