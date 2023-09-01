4a09df58171b5454fab4ff59b426a6c78dbf5cbb
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-01 13:52:13 +0200

    Add format time util

---
+++ src/utils/formatTime.ts
@@ -0,0 +1,15 @@
+export const formatTime = (milliseconds: number, roundMilliseconds: boolean = true) => {
+    const minutes = Math.floor(milliseconds / 60000);
+    milliseconds %= 60000;
+    const seconds = Math.floor(milliseconds / 1000);
+    milliseconds %= 1000;
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes.toString();
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds.toString();
+    const formattedMilliseconds =
+        milliseconds < 10 ? `00${milliseconds}` : milliseconds < 100 ? `0${milliseconds}` : milliseconds.toString();
+
+    return `${formattedMinutes}:${formattedSeconds}.${
+        roundMilliseconds ? Math.round(+formattedMilliseconds / 10) : formattedMilliseconds
+    }`;
+};
