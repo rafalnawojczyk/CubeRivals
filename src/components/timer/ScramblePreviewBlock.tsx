9ce72dd275d6dbf7d2e0a0da992b506204a5730e
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-02 22:20:16 +0200

    Add scramble generation logic and layout

---
+++ src/components/timer/ScramblePreviewBlock.tsx
@@ -0,0 +1,89 @@
+import { useContext, useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { generateScramble } from '../../utils/generateScramble';
+import { TimerSettingsContext } from '../../store/timer-settings-context';
+import { CubeType } from '../../models/cubes';
+import { useColors } from '../../hooks/useColors';
+import { DIMENSIONS, FONTS, PADDING } from '../../styles/base';
+import { IconButton } from '../UI/IconButton';
+import { AddScrambleModal } from './modals/AddScrambleModal';
+
+export const ScramblePreviewBlock = ({
+    onChangeScramble,
+    scramble,
+}: {
+    onChangeScramble: (scramble: string) => void;
+    scramble: string;
+}) => {
+    const { timerSettings } = useContext(TimerSettingsContext);
+    const [showAddScrambleModal, setShowAddScrambleModal] = useState(false);
+    const getColor = useColors();
+
+    const onAddOwnScramble = (scramble: string) => {
+        onChangeScramble(scramble);
+    };
+
+    const onShowAddScramble = () => {
+        setShowAddScrambleModal(true);
+    };
+
+    const onRegenerateScramble = () => {
+        const scr = generateScramble(timerSettings.cube);
+        onChangeScramble(scr);
+    };
+
+    useEffect(() => {
+        onRegenerateScramble();
+    }, []);
+
+    return (
+        <>
+            <AddScrambleModal
+                onAddScramble={onAddOwnScramble}
+                onClose={() => setShowAddScrambleModal(false)}
+                showModal={showAddScrambleModal}
+            />
+            <View style={styles.outerContainer}>
+                <View style={styles.container}>
+                    <Text style={[styles.scramble, { color: getColor('gray100') }]}>{scramble}</Text>
+                    <View style={styles.buttonsContainer}>
+                        <IconButton
+                            icon="pencil-outline"
+                            size={FONTS.lg}
+                            color={getColor('gray100')}
+                            onPress={onShowAddScramble}
+                        />
+                        <IconButton
+                            icon="refresh-outline"
+                            size={FONTS.lg}
+                            color={getColor('gray100')}
+                            onPress={onRegenerateScramble}
+                        />
+                    </View>
+                </View>
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        width: DIMENSIONS.fullWidth * 0.95,
+    },
+    buttonsContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        gap: PADDING.sm,
+    },
+    scramble: {
+        fontSize: FONTS.md,
+        textAlign: 'center',
+    },
+});
---
+++ src/components/timer/modals/AddScrambleModal.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+import { CustomModal } from '../../UI/modal/CustomModal';
+import { DIMENSIONS, FONTS, PADDING } from '../../../styles/base';
+import { useColors } from '../../../hooks/useColors';
+import { useTranslation } from '../../../hooks/useTranslation';
+
+interface AddScrambleModalProps {
+    showModal: boolean;
+    onClose: () => void;
+    onAddScramble: (note: string) => void;
+}
+
+export const AddScrambleModal = ({ showModal, onClose, onAddScramble }: AddScrambleModalProps) => {
+    const [scrambleInput, setScrambleInput] = useState('');
+    const getColor = useColors();
+    const translate = useTranslation();
+
+    return (
+        <CustomModal isVisible={showModal} onClose={onClose} title={translate('addOwnScramble')} size="lg">
+            <TextInput
+                value={scrambleInput}
+                onChangeText={setScrambleInput}
+                multiline={true}
+                maxLength={250}
+                style={[styles.textInput, { backgroundColor: getColor('gray100') }]}
+                textAlignVertical="top"
+                textAlign="center"
+            />
+            <CustomModal.ButtonsContainer>
+                <CustomModal.Button type="cancel" onPress={onClose}>
+                    {translate('cancel')}
+                </CustomModal.Button>
+                <CustomModal.Button type="primary" onPress={() => onAddScramble(scrambleInput)}>
+                    {translate('addCommentConfirm')}
+                </CustomModal.Button>
+            </CustomModal.ButtonsContainer>
+        </CustomModal>
+    );
+};
+
+const styles = StyleSheet.create({
+    textInput: {
+        width: '100%',
+        borderRadius: 2,
+        minHeight: DIMENSIONS.fullHeight * 0.2,
+        padding: PADDING.sm,
+        fontSize: FONTS.md,
+    },
+});
