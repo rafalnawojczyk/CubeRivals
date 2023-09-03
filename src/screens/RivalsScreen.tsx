9f75e5dd4347391f11276560a9394eb27f6abdb7
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-03 14:53:00 +0200

    Add 'coming soon' screen animation to rivals

--- src/screens/RivalsScreen.tsx
+++ src/screens/RivalsScreen.tsx
@@ -1,10 +1,39 @@
-import { SafeAreaView, Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import { SafeAreaCard } from '../components/UI/SafeAreaCard';
+import LottieView from 'lottie-react-native';
+import { useColors } from '../hooks/useColors';
+import { FONTS } from '../styles/base';
+
+const LOTTIE_JSON = require('../assets/rocket-animation.json');
 
 export const RivalsScreen = () => {
+    const getColor = useColors();
+
     return (
         <SafeAreaCard>
-            <Text>RIVALS SCREEN</Text>
+            <View style={[styles.container, { backgroundColor: getColor('background') }]}>
+                <Text style={[styles.comingSoonText, { color: getColor('text') }]}>COMING SOON</Text>
+                <LottieView
+                    style={{
+                        width: '100%',
+                    }}
+                    speed={0.3}
+                    autoPlay
+                    source={LOTTIE_JSON}
+                    loop={true}
+                />
+            </View>
         </SafeAreaCard>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    comingSoonText: {
+        fontSize: FONTS.xl,
+        textAlign: 'center',
+    },
+});
