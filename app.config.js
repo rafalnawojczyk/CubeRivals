c69a376d70842136e7c273ddca7c6bed59e5d551
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-05 19:18:39 +0200

    Add firebase and configure app

---
+++ app.config.js
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+
+export default {
+    expo: {
+        name: 'Cube-rivals',
+        slug: 'Cube-rivals',
+        version: '1.0.0',
+        orientation: 'portrait',
+        icon: './assets/icon.png',
+        userInterfaceStyle: 'automatic',
+        splash: {
+            image: './assets/splash.png',
+            resizeMode: 'contain',
+            backgroundColor: '#ffffff',
+        },
+        assetBundlePatterns: ['**/*'],
+        ios: {
+            supportsTablet: true,
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: './assets/adaptive-icon.png',
+                backgroundColor: '#ffffff',
+            },
+            package: 'com.anonymous.Cuberivals',
+        },
+        web: {
+            favicon: './assets/favicon.png',
+        },
+        plugins: ['expo-localization'],
+
+        extra: {
+            firebaseApiKey: process.env.FIREBASE_API,
+            firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL,
+            firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+            firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            firebaseAppId: process.env.FIREBASE_APP_ID,
+            firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        },
+    },
+};
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,6 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
+        plugins: ['@realm/babel-plugin', ['@babel/plugin-proposal-decorators', { legacy: true }]],
     };
 };
