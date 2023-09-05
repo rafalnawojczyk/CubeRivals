820fdc8f53753242ea748de903b22d7d00b7a6d6
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-05 20:53:45 +0200

    Update todo structure and add tasks

--- todo.js
+++ todo.js
@@ -1,33 +1,35 @@
 // ********************************** HIGH **********************************
-// TODO: list of solves which show list, each item acn be clicked and modal with solve editing shows. User can edit everything there
-// TODO: add "Warmup" mode where solves are not saved
-// TODO: add image of scrambled cube
+// 1. list of solves which show list, each item acn be clicked and modal with solve editing shows. User can edit everything there
+// 2. add "Warmup" mode where solves are not saved
+// 3. add image of scrambled cube
 
 // ********************************** MODERATE **********************************
-// TODO: Statistics based on days, not counting every solve?
-// TODO: set ready when front camera is covered?
-// TODO: set finished when there is any gyro movement
-// TODO: Export to CSV/ import/export compatible with csTimer
-// TODO: maybe add option to create own colors for whole app? and background image for timer screen? It should be dimmed by default
-// TODO: add "custom" cube option so users can add their own cube without scramble/ Maybe add "custom" and users will place there their sessions?
-// TODO: add by default more categories, like bld, multi blind
-// TODO: maybe create one big context with all data and reducers or REDUX? Or create one component that will provide all at once
-// TODO: work on implementing a great time adding feature, so it will be easy for ppl to use stackmats
-// TODO: read about connecting bluetooth cubes/stackmat timers to app?
-// TODO: ability to use the same scramble as before solve?
-// TODO: ability to change amount of moves in scramble?
-// TODO: history of PB's - how they changed/fast overtime
+// 1. Statistics based on days, not counting every solve?
+// 2. set ready when front camera is covered?
+// 3. set finished when there is any gyro movement
+// 4. Export to CSV/ import/export compatible with csTimer
+// 5. maybe add option to create own colors for whole app? and background image for timer screen? It should be dimmed by default
+// 6. add "custom" cube option so users can add their own cube without scramble/ Maybe add "custom" and users will place there their sessions?
+// 7. add by default more categories, like bld, multi blind
+// 8. maybe create one big context with all data and reducers or REDUX? Or create one component that will provide all at once
+// 9. work on implementing a great time adding feature, so it will be easy for ppl to use stackmats
+// 10. read about connecting bluetooth cubes/stackmat timers to app?
+// 11. ability to use the same scramble as before solve?
+// 12. ability to change amount of moves in scramble?
+// 13. history of PB's - how they changed/fast overtime
+// 14. Add Avatar(maybe some default options), Nicknames(using Firestore)
+// 15. Two way binding between firestore and realm database - userId/firebase UID
 
 // ********************************** LOW **********************************
-// TODO: think about Adding sounds to app>?
-// TODO: Timer numbers turns to yellow/red when you exceeds your avg5/avg12 time?
-// TODO: Timer shows only full seconds, without any milliseconds
-// TODO: maybe 3 presets for how long to tap screen to start solve?
-// TODO: Read about not dimming screen/ not blocking screen while in use
-// TODO: Splash screen should be really looking good. It should have some kind of fading effect, animation should start when all assets are done fetching. Should use animated to do this.
-// TODO: OLL/PLL trainer, where user can choose one of X algs for each case, add his own alg, train one/all selected at the same time
-// TODO: More advanced trainers like  VLS cases, ZBLL, and OLLCP
-// TODO: Multiphase timing feature, which lets you count each step in solve
-// TODO: Customization of AVG/MEANS to exact numbers which user chooses, not only to predefined ones
-// TODO: Daily statistics of solves, which shows avg of solves today
-// TODO: Scramble trainer?
+// 1. think about Adding sounds to app>?
+// 2. Timer numbers turns to yellow/red when you exceeds your avg5/avg12 time?
+// 3. Timer shows only full seconds, without any milliseconds
+// 4. maybe 3 presets for how long to tap screen to start solve?
+// 5. Read about not dimming screen/ not blocking screen while in use
+// 6. Splash screen should be really looking good. It should have some kind of fading effect, animation should start when all assets are done fetching. Should use animated to do this.
+// 7. OLL/PLL trainer, where user can choose one of X algs for each case, add his own alg, train one/all selected at the same time
+// 8. More advanced trainers like  VLS cases, ZBLL, and OLLCP
+// 9. Multiphase timing feature, which lets you count each step in solve
+// 10. Customization of AVG/MEANS to exact numbers which user chooses, not only to predefined ones
+// 11. Daily statistics of solves, which shows avg of solves today
+// 12. Scrambling trainer?
