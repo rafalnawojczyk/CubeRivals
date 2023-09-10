61c67d45708d0c3b22474072cc9c6ce4e5ec6459
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-11 00:04:40 +0200

    Temp improve times list placement

--- src/components/timesList/TimesList.tsx
+++ src/components/timesList/TimesList.tsx
@@ -1,27 +1,62 @@
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { TimeListItem } from './TimeListItem';
 import { Solve } from '../../models/realm-models/SolveSchema';
 import Realm from 'realm';
 import { EmptyFallbackAnimation } from '../EmptyFallbackAnimation';
+import { DIMENSIONS, PADDING } from '../../styles/base';
 
 interface TimesListProps {
     data: Realm.List<Solve> | undefined;
 }
 
 export const TimesList = ({ data }: TimesListProps) => {
+    const dataLength = data ? data.length : 0;
+    const addLastItem = dataLength % 3 === 2;
+
+    const dataToShow = [];
+
+    if (data) {
+        dataToShow.push(...data);
+    }
+
+    if (addLastItem) {
+        dataToShow.push({ _id: Math.random(), isEmptyItem: true });
+    }
+
     return (
         <>
             {!data || data.length === 0 ? (
                 <EmptyFallbackAnimation />
             ) : (
                 <FlatList
-                    data={data}
+                    data={dataToShow}
                     style={{ flex: 1 }}
                     contentContainerStyle={{ flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between' }}
                     keyExtractor={item => item._id.toString()}
-                    renderItem={({ item }) => <TimeListItem result={item} />}
+                    renderItem={({ item, index }) => {
+                        // @ts-ignore
+                        if (item?.isEmptyItem) {
+                            return <View style={styles.emptyContainer}></View>;
+                        }
+                        // @ts-ignore
+                        return <TimeListItem result={item} />;
+                    }}
                 />
             )}
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    emptyContainer: {
+        flex: 1,
+        height: DIMENSIONS.fullHeight * 0.08,
+        marginBottom: PADDING.md,
+        borderRadius: 9,
+        minWidth: '30%',
+        maxWidth: '100%',
+        justifyContent: 'space-between',
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+    },
+});
