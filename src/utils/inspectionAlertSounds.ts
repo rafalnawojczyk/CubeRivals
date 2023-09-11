54282976030fc07aa71df1b2edc133dcd462f4da
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-11 17:18:04 +0200

    Add inspection alert sounds

---
+++ src/utils/inspectionAlertSounds.ts
@@ -0,0 +1,10 @@
+export const INSPECTION_ALERT_SOUNDS = [
+    require('../../assets/sounds/inspection-alert-1.wav'),
+    require('../../assets/sounds/inspection-alert-2.wav'),
+    require('../../assets/sounds/inspection-alert-3.wav'),
+    require('../../assets/sounds/inspection-alert-4.wav'),
+    require('../../assets/sounds/inspection-alert-5.wav'),
+    require('../../assets/sounds/inspection-alert-6.wav'),
+    require('../../assets/sounds/inspection-alert-7.wav'),
+    require('../../assets/sounds/inspection-alert-8.wav'),
+];
