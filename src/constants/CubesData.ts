b5753de98a5234f10efb5ca4e0d1f8621fef82eb
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-09-14 21:16:05 +0200

    Organize cube icons

--- src/constants/CubesData.ts
+++ src/constants/CubesData.ts
@@ -1,14 +1,14 @@
 import { CubeDataInterface } from '../models/cubes';
 
 export const CUBES_DATA: CubeDataInterface[] = [
-    { fullName: '3x3x3', icon: require('../../assets/333.png'), id: '333' },
-    { fullName: '2x2x2', icon: require('../../assets/222.png'), id: '222' },
-    { fullName: '4x4x4', icon: require('../../assets/444.png'), id: '444' },
-    { fullName: '5x5x5', icon: require('../../assets/555.png'), id: '555' },
-    { fullName: '6x6x6', icon: require('../../assets/666.png'), id: '666' },
-    { fullName: 'Skewb', icon: require('../../assets/skewb.png'), id: 'skewb' },
-    { fullName: 'Megaminx', icon: require('../../assets/minx.png'), id: 'minx' },
-    { fullName: 'Pyraminx', icon: require('../../assets/pyram.png'), id: 'pyram' },
-    { fullName: 'Square-1', icon: require('../../assets/sq1.png'), id: 'sq1' },
-    { fullName: 'Clock', icon: require('../../assets/clock.png'), id: 'clock' },
+    { fullName: '3x3x3', icon: require('../../assets/icons/333.png'), id: '333' },
+    { fullName: '2x2x2', icon: require('../../assets/icons/222.png'), id: '222' },
+    { fullName: '4x4x4', icon: require('../../assets/icons/444.png'), id: '444' },
+    { fullName: '5x5x5', icon: require('../../assets/icons/555.png'), id: '555' },
+    { fullName: '6x6x6', icon: require('../../assets/icons/666.png'), id: '666' },
+    { fullName: 'Skewb', icon: require('../../assets/icons/skewb.png'), id: 'skewb' },
+    { fullName: 'Megaminx', icon: require('../../assets/icons/minx.png'), id: 'minx' },
+    { fullName: 'Pyraminx', icon: require('../../assets/icons/pyram.png'), id: 'pyram' },
+    { fullName: 'Square-1', icon: require('../../assets/icons/sq1.png'), id: 'sq1' },
+    { fullName: 'Clock', icon: require('../../assets/icons/clock.png'), id: 'clock' },
 ];
