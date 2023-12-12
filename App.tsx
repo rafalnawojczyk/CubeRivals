b4406423f1739e9e077704d737bbecd26f2a06dc
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-12-12 23:11:40 +0100

    Add themeFroMStorage func to app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
 import { useCallback, useState, useRef, useEffect } from 'react';
-import { StyleSheet, View, Animated, Easing } from 'react-native';
+import { StyleSheet, View, Animated, Easing, useColorScheme } from 'react-native';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { Navigation } from './src/navigation/Navigation';
 import { CubeAnimation } from './src/components/CubeAnimation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useThemeStore } from './src/store/themeStore';
 
 SplashScreen.preventAutoHideAsync().catch(() => {});
 
@@ -23,6 +25,21 @@ export default function App() {
     const [fontsReady, setFontsReady] = useState(false);
     const [isAppReady, setAppReady] = useState(false);
     const animationProgress = useRef(new Animated.Value(0));
+    const deviceTheme = useColorScheme();
+
+    const getThemeFromStorage = async () => {
+        try {
+            const savedTheme = await AsyncStorage.getItem('theme');
+
+            if (savedTheme) {
+                useThemeStore.setState({ isDarkTheme: savedTheme === 'dark' });
+            } else {
+                useThemeStore.setState({ isDarkTheme: deviceTheme === 'dark' });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     useEffect(() => {
         const loadFonts = async () => {
@@ -32,7 +49,7 @@ export default function App() {
         };
 
         loadFonts();
-
+        getThemeFromStorage();
         Animated.loop(
             Animated.timing(animationProgress.current, {
                 toValue: 1,
