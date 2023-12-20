a6d6921f372f81629ee4bddbbec0b9fa9be1ef98
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-12-20 23:23:59 +0100

    Update colors and design system

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,47 +1,99 @@
-export interface ColorsInterface {
-    background: string;
-    primary200: string;
-    primary500: string;
-    primary800: string;
-    secondary: string;
-    tertiary: string;
-    gray800: string;
-    gray700: string;
-    gray400: string;
-    gray100: string;
-    text: string;
-    textInverted: string;
-    error: string;
-}
+export type ColorsInterface = typeof DARK_THEME;
+
+export const DARK_THEME = {
+    // PRIMARY
+    primary100: '#FFF1DA',
+    primary200: '#FEE1B2',
+    primary300: '#FED28A',
+    primary400: '#FDC262',
+    primary500: '#FDB339',
+    primary600: '#FCA311',
+    primary700: '#D28403',
+    primary800: '#9B6102',
+    primary900: '#633E01',
+
+    // GRAY
+    gray100: '#FFFFFF',
+    gray200: '#FEFEFE',
+    gray300: '#EAEAEA',
+    gray400: '#D5D5D5',
+    gray500: '#C1C1C1',
+    gray600: '#A5A5A5',
+    gray700: '#898989',
+    gray800: '#5C6076', // buttons on dark version?
+    gray900: '#515151',
+    // TEXT
+    text: '#ffffff',
+    white: '#ffffff',
+    accentLight: '#787DAF',
+    background100: '#212A52',
+    background: '#151837',
+    backgroundLight: '#1B2344',
+
+    error: '#C25451',
+} as const;
 
 export const LIGHT_THEME: ColorsInterface = {
-    background: '#232838',
-    primary200: '#8166ec',
-    primary500: '#582fff',
-    primary800: '#2d1686',
-    secondary: '#7edbe0',
-    tertiary: '#ec9898',
-    gray800: '#1d1f24',
-    gray700: '#23252b',
-    gray400: '#676d75',
-    gray100: '#ccd2da',
-    text: '#ffffff',
-    textInverted: '#202229',
-    error: '#E63756',
-};
+    // PRIMARY
+    primary100: '#FFF1DA',
+    primary200: '#FEE1B2',
+    primary300: '#FED28A',
+    primary400: '#FDC262',
+    primary500: '#FDB339',
+    primary600: '#FCA311',
+    primary700: '#D28403',
+    primary800: '#9B6102',
+    primary900: '#633E01',
 
-export const DARK_THEME: ColorsInterface = {
-    background: '#232838',
-    primary200: '#8166ec',
-    primary500: '#582fff',
-    primary800: '#2d1686',
-    secondary: '#7edbe0',
-    tertiary: '#ec9898',
-    gray800: '#1d1f24',
-    gray700: '#23252b',
-    gray400: '#676d75',
-    gray100: '#ccd2da',
+    // GRAY
+    gray100: '#FFFFFF',
+    gray200: '#FEFEFE',
+    gray300: '#EAEAEA',
+    gray400: '#D5D5D5',
+    gray500: '#C1C1C1',
+    gray600: '#A5A5A5',
+    gray700: '#898989',
+    gray800: '#5C6076', // buttons on dark version?
+    gray900: '#515151',
+    // TEXT
     text: '#ffffff',
-    textInverted: '#202229',
-    error: '#E63756',
-};
+    white: '#ffffff',
+    accentLight: '#787DAF',
+    background100: '#212A52',
+    background: '#151837',
+    backgroundLight: '#1B2344',
+
+    error: '#C25451',
+} as const;
+
+// export const LIGHT_THEME: ColorsInterface = {
+//     background: '#232838',
+//     primary200: '#8166ec',
+//     primary500: '#582fff',
+//     primary800: '#2d1686',
+//     secondary: '#7edbe0',
+//     tertiary: '#ec9898',
+//     gray800: '#1d1f24',
+//     gray700: '#23252b',
+//     gray400: '#676d75',
+//     gray100: '#ccd2da',
+//     text: '#ffffff',
+//     textInverted: '#202229',
+//     error: '#E63756',
+// };
+
+// export const DARK_THEME: ColorsInterface = {
+//     background: '#232838',
+//     primary200: '#8166ec',
+//     primary500: '#582fff',
+//     primary800: '#2d1686',
+//     secondary: '#7edbe0',
+//     tertiary: '#ec9898',
+//     gray800: '#1d1f24',
+//     gray700: '#23252b',
+//     gray400: '#676d75',
+//     gray100: '#ccd2da',
+//     text: '#ffffff',
+//     textInverted: '#202229',
+//     error: '#E63756',
+// };
