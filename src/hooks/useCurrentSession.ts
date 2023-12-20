190bee29a775084e3c64d0515a2903112f4fe554
Rafał Nawojczyk <102222064+rafalnawojczyk@users.noreply.github.com>
2023-12-20 23:26:45 +0100

    Modify useSession hook

--- src/hooks/useCurrentSession.ts
+++ src/hooks/useCurrentSession.ts
@@ -1,18 +1,26 @@
-import { useQuery, useRealm } from '@realm/react';
+import { useQuery, useRealm, useUser } from '@realm/react';
 import { useTimerSettingsStore } from '../store/timerSettingsStore';
 import { Session } from '../models/realm-models/SessionSchema';
 import { useEffect } from 'react';
 import { Solve } from '../models/realm-models/SolveSchema';
+import { addSession } from '../models/utils';
+import { useTranslation } from './useTranslation';
 
 export const useCurrentSession = () => {
+    const trans = useTranslation();
     const cube = useTimerSettingsStore(state => state.cube);
+    const user = useUser();
 
     const solves = useQuery(Solve);
 
     const realm = useRealm();
     const sessions = useQuery(Session, collection => collection.filtered('cube == $0', cube), [cube]);
 
-    const session = sessions.sorted('used', true)[0];
+    let session = sessions.sorted('used', true)[0];
+
+    if (!session) {
+        session = addSession(trans('defaultSessionName'), cube, user.id, realm);
+    }
 
     useEffect(() => {
         realm.subscriptions.update(mutableSubs => {
